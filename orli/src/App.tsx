ddd81d9e0a473a739668ea348f919f9117803091
calpwin <calpwin@gmail.com>
2022-04-22 21:49:47 +0300

    improve

--- orli/src/App.tsx
+++ orli/src/App.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { CElementService } from './services/celement.service';
+import * as PIXI from 'pixi.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload !!.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  render() {
+    return <div id="editor-wrapper"></div>;
+  }
+
+  componentDidMount() {
+    const cElementService = new CElementService();
+
+    // The application will create a renderer using WebGL, if possible,
+    // with a fallback to a canvas render. It will also setup the ticker
+    // and the root stage PIXI.Container
+    const app = new PIXI.Application({ backgroundColor: 0xffffff });
+
+    const { renderer } = app;
+
+    const rootEl = document.getElementById("editor-wrapper") as HTMLElement;
+    // (renderer as Renderer).addSystem(<any>EventSystem, 'events');
+    app.resizeTo = rootEl;
+
+    renderer.render(app.stage);
+    // The application will create a canvas element for you that you
+    // can then insert into the DOM
+    rootEl.appendChild(app.view);
+
+    const container = cElementService.createCElement(0, 0, 400, 400, 0x99aaaa);
+
+    const cel1 = cElementService.createCElement(0, 20, 60, 30, 0x66ccff);
+    cel1.graphics.x = container.graphics.x;
+    container.addChild(cel1);
+
+    const cel2 = cElementService.createCElement(0, 50, 60, 30, 0x66ccff);
+    cel2.graphics.x = container.graphics.x;
+    container.addChild(cel2);
+
+    // const circle = container.graphics.drawCircle(0, 0, 10);
+    // circle.beginFill(0x66ccff);
+    // circle.endFill();
+    // container.graphics.addChild(circle);
+
+    app.stage.addChild(container.graphics);
+  }
 }
-
-export default App;
---
+++ orli/src/base/ioc-types.ts
@@ -0,0 +1,5 @@
+const IocTypes = {
+    CElementService: Symbol.for("CElementService"),
+};
+
+export { IocTypes };
